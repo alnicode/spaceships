80f53a3f02d3ecb3036f1b2536dda97a3c09e698
Alben Bustamante <albenbustamante@gmail.com>
2022-07-15 11:16:49 -0500

    Define a inventory constant

---
+++ src/main/java/com/alnicode/spaceships/constants/InventoryConstants.java
@@ -0,0 +1,12 @@
+package com.alnicode.spaceships.constants;
+
+/**
+ * The {@code Inventory} constants.
+ *
+ * @author Alben Bustamante
+ * @version 1.0
+ * @since 1.0
+ */
+public interface InventoryConstants {
+    int NAME_LENGTH = 80;
+}
