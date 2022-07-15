a4ef826547e508ea90ac21e3e7e2efa9eab9b59f
Alben Bustamante <albenbustamante@gmail.com>
2022-07-15 12:07:09 -0500

    Add repository annotation

--- src/main/java/com/alnicode/spaceships/persistence/repository/SpaceShuttleRepository.java
+++ src/main/java/com/alnicode/spaceships/persistence/repository/SpaceShuttleRepository.java
@@ -2,6 +2,7 @@ package com.alnicode.spaceships.persistence.repository;
 
 import com.alnicode.spaceships.persistence.entity.SpaceShuttle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * The {@link SpaceShuttle} repository.
@@ -10,5 +11,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @version 1.0
  * @since 1.0
  */
+@Repository
 public interface SpaceShuttleRepository extends JpaRepository<SpaceShuttle, Long> {
 }
