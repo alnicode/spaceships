ef528037463f82f974f886d6cf664c3ee19a391b
Alben Bustamante <albenbustamante@gmail.com>
2022-07-15 13:18:20 -0500

    Create the robotic spaceship dto models

---
+++ src/main/java/com/alnicode/spaceships/domain/dto/robotic/RoboticSpaceshipRequest.java
@@ -0,0 +1,29 @@
+package com.alnicode.spaceships.domain.dto.robotic;
+
+import com.alnicode.spaceships.domain.dto.thrust.ThrustSpaceshipRequest;
+import lombok.Getter;
+
+/**
+ * The {@code RoboticSpaceship} request model dto.
+ *
+ * @author Alben Bustamante
+ * @since 1.0
+ * @version 1.0
+ */
+@Getter
+public class RoboticSpaceshipRequest extends ThrustSpaceshipRequest {
+
+    private final double speed;
+    private final String speedMeasure;
+    private final String destinyOrbit;
+
+    public RoboticSpaceshipRequest(final double weight, final String weightMeasure, final int height, final String heightMeasure,
+                               final String mission, final String fuel, final String state, final double thrust, final String thrustMeasure,
+                               final double speed, final String speedMeasure, final String destinyOrbit) {
+        super(weight, weightMeasure, height, heightMeasure, mission, fuel, state, thrust, thrustMeasure);
+        this.speed = speed;
+        this.speedMeasure = speedMeasure;
+        this.destinyOrbit = destinyOrbit;
+    }
+
+}
