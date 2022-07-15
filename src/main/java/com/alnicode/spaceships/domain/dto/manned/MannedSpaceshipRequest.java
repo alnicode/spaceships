7ff48f98a64a8a4dc116486c1f8eef16dcf9ef35
Alben Bustamante <albenbustamante@gmail.com>
2022-07-15 13:12:57 -0500

    Create the manned spaceship dto models

---
+++ src/main/java/com/alnicode/spaceships/domain/dto/manned/MannedSpaceshipRequest.java
@@ -0,0 +1,29 @@
+package com.alnicode.spaceships.domain.dto.manned;
+
+import com.alnicode.spaceships.domain.dto.spacecraft.SpacecraftRequest;
+import lombok.Getter;
+
+/**
+ * The {@code MannedSpaceship} request model dto.
+ *
+ * @author Alben Bustamante
+ * @since 1.0
+ * @version 1.0
+ */
+@Getter
+public class MannedSpaceshipRequest extends SpacecraftRequest {
+
+    private final int earthOrbit;
+    private final String earthOrbitMeasure;
+    private final int peopleCapacity;
+
+    public MannedSpaceshipRequest(final double weight, final String weightMeasure, final int height, final String heightMeasure,
+                                  final String mission, final String fuel, final String state, final int earthOrbit,
+                                  final String earthOrbitMeasure, final int peopleCapacity) {
+        super(weight, weightMeasure, height, heightMeasure, mission, fuel, state);
+        this.earthOrbit = earthOrbit;
+        this.earthOrbitMeasure = earthOrbitMeasure;
+        this.peopleCapacity = peopleCapacity;
+    }
+
+}
---
+++ src/main/java/com/alnicode/spaceships/domain/dto/manned/MannedSpaceshipResponse.java
@@ -0,0 +1,26 @@
+package com.alnicode.spaceships.domain.dto.manned;
+
+import com.alnicode.spaceships.domain.dto.spacecraft.SpacecraftResponse;
+import lombok.Getter;
+
+/**
+ * The {@code MannedSpaceship} response model dto.
+ *
+ * @author Alben Bustamante
+ * @since 1.0
+ * @version 1.0
+ */
+@Getter
+public class MannedSpaceshipResponse extends SpacecraftResponse {
+
+    private final String earthOrbit;
+    private final String peopleCapacity;
+
+    public MannedSpaceshipResponse(final long id, final String weight, final String height, final String heightMeasure, final String mission,
+                                   final String fuel, final String state, final String earthOrbit, final String peopleCapacity) {
+        super(id, weight, height, heightMeasure, mission, fuel, state);
+        this.earthOrbit = earthOrbit;
+        this.peopleCapacity = peopleCapacity;
+    }
+
+}
