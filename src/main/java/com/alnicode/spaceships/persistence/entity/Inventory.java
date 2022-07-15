07f5eb004aa8f120cab72ed23c7ce9928177e1ed
Alben Bustamante <albenbustamante@gmail.com>
2022-07-15 11:24:02 -0500

    Define the relationships

--- src/main/java/com/alnicode/spaceships/persistence/entity/Inventory.java
+++ src/main/java/com/alnicode/spaceships/persistence/entity/Inventory.java
@@ -1,11 +1,16 @@
 package com.alnicode.spaceships.persistence.entity;
 
 import com.alnicode.spaceships.constants.InventoryConstants;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -32,5 +37,21 @@ public class Inventory {
 
     @Column(nullable = false, length = InventoryConstants.NAME_LENGTH)
     private String name;
+
+    @OneToMany(mappedBy = "inventory", cascade = {CascadeType.ALL})
+    private Set<Spacecraft> spacecrafts = new HashSet<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Inventory inventory = (Inventory) o;
+        return Objects.equals(id, inventory.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 
 }
--- src/main/java/com/alnicode/spaceships/persistence/entity/Spacecraft.java
+++ src/main/java/com/alnicode/spaceships/persistence/entity/Spacecraft.java
@@ -1,10 +1,14 @@
 package com.alnicode.spaceships.persistence.entity;
 
 import com.alnicode.spaceships.constants.SpacecraftConstants;
+import java.util.Objects;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -38,5 +42,21 @@ public class Spacecraft {
 
     @Column(nullable = false, length = SpacecraftConstants.STATE_LENGTH)
     private String state;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private Inventory inventory;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Spacecraft that = (Spacecraft) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 
 }
