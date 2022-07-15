0a39cdd0774a31ea927a7a3333fe958fc71a9711
Alben Bustamante <albenbustamante@gmail.com>
2022-07-15 12:27:05 -0500

    Define some abstract methods

---
+++ src/main/java/com/alnicode/spaceships/domain/Image.java
@@ -0,0 +1,17 @@
+package com.alnicode.spaceships.domain;
+
+/**
+ * The spaceship capture and send images.
+ *
+ * @author Alben Bustamante
+ * @since 1.0
+ * @version 1.0
+ */
+public interface Image {
+
+    /**
+     * The spaceship send the captured images.
+     */
+    void sendImages();
+
+}
