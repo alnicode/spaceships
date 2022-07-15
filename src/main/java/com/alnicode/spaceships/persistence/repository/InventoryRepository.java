c97c6211b093f5203d38a59d2bfe2cd21a520ad4
Alben Bustamante <albenbustamante@gmail.com>
2022-07-15 15:36:39 -0500

    Add some inventory methods

--- src/main/java/com/alnicode/spaceships/persistence/repository/InventoryRepository.java
+++ src/main/java/com/alnicode/spaceships/persistence/repository/InventoryRepository.java
@@ -1,6 +1,7 @@
 package com.alnicode.spaceships.persistence.repository;
 
 import com.alnicode.spaceships.persistence.entity.Inventory;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,4 +14,21 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
+    /**
+     * Find an inventory by the name.
+     *
+     * @param name the name to search.
+     * @return an optional of the inventory found.
+     */
+    Optional<Inventory> findByName(String name);
+
+    /**
+     * Find an inventory by the spacecraft id.
+     *
+     * @param id the spacecraft id to search.
+     * @return an optional of the inventory found.
+     */
+    Optional<Inventory> findBySpacecraftsId(long id);
+
 }
