c1ae8ebd09c60adfe08b0e3e8c96e4362bcf36f2
Alben Bustamante <albenbustamante@gmail.com>
2022-07-15 13:08:33 -0500

    Create the space shuttle dto models

---
+++ src/main/java/com/alnicode/spaceships/domain/dto/shuttle/SpaceShuttleRequest.java
@@ -0,0 +1,29 @@
+package com.alnicode.spaceships.domain.dto.shuttle;
+
+import com.alnicode.spaceships.domain.dto.thrust.ThrustSpaceshipRequest;
+import lombok.Getter;
+
+/**
+ * The {@code SpaceShuttle} request model dto.
+ *
+ * @author Alben Bustamante
+ * @since 1.0
+ * @version 1.0
+ */
+@Getter
+public class SpaceShuttleRequest extends ThrustSpaceshipRequest {
+
+    private final int transport;
+    private final String transportMeasure;
+    private final String power;
+
+    public SpaceShuttleRequest(final double weight, final String weightMeasure, final int height, final String heightMeasure,
+                               final String mission, final String fuel, final String state, final double thrust, final String thrustMeasure,
+                               final int transport, final String transportMeasure, final String power) {
+        super(weight, weightMeasure, height, heightMeasure, mission, fuel, state, thrust, thrustMeasure);
+        this.transport = transport;
+        this.transportMeasure = transportMeasure;
+        this.power = power;
+    }
+
+}
---
+++ src/main/java/com/alnicode/spaceships/domain/dto/shuttle/SpaceShuttleResponse.java
@@ -0,0 +1,27 @@
+package com.alnicode.spaceships.domain.dto.shuttle;
+
+import com.alnicode.spaceships.domain.dto.thrust.ThrustSpaceshipResponse;
+import lombok.Getter;
+
+/**
+ * The {@code SpaceShuttle} response model dto.
+ *
+ * @author Alben Bustamante
+ * @since 1.0
+ * @version 1.0
+ */
+@Getter
+public class SpaceShuttleResponse extends ThrustSpaceshipResponse {
+
+    private final String transport;
+    private final String power;
+
+    public SpaceShuttleResponse(final long id, final String weight, final String height, final String heightMeasure,
+                                final String mission, final String fuel, final String state, final String thrust,
+                                final String transport, final String power) {
+        super(id, weight, height, heightMeasure, mission, fuel, state, thrust);
+        this.transport = transport;
+        this.power = power;
+    }
+
+}
