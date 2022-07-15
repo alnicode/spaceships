21b8f1a75437cb14b2217fb43d34a39978884995
Alben Bustamante <albenbustamante@gmail.com>
2022-07-15 16:39:01 -0500

    Define the service templates

---
+++ src/main/java/com/alnicode/spaceships/domain/service/RoboticSpaceshipService.java
@@ -0,0 +1,33 @@
+package com.alnicode.spaceships.domain.service;
+
+import com.alnicode.spaceships.domain.dto.robotic.RoboticSpaceshipRequest;
+import com.alnicode.spaceships.domain.dto.robotic.RoboticSpaceshipResponse;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * The robotic spaceship service template.
+ *
+ * @author Alben Bustamante
+ * @since 1.0
+ * @version 1.0
+ */
+public interface RoboticSpaceshipService extends ThrustSpaceshipService<RoboticSpaceshipRequest, RoboticSpaceshipResponse> {
+
+    /**
+     * Find a robotic spaceship by the speed.
+     *
+     * @param speed the speed to search.
+     * @return an optional of the spaceships found.
+     */
+    Optional<List<RoboticSpaceshipResponse>> findBySpeed(String speed);
+
+    /**
+     * Find a robotic spaceship by the destiny orbit.
+     *
+     * @param destinyOrbit the destiny orbit to search.
+     * @return an optional of the spaceships found.
+     */
+    Optional<List<RoboticSpaceshipResponse>> findByDestinyOrbit(String destinyOrbit);
+
+}
