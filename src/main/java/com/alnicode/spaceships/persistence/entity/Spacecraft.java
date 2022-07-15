ffcb93a0473d703938aed5bd245ebc7604ccfbae
Alben Bustamante <albenbustamante@gmail.com>
2022-07-15 11:44:25 -0500

    Change the inheritance strategy

--- src/main/java/com/alnicode/spaceships/persistence/entity/Spacecraft.java
+++ src/main/java/com/alnicode/spaceships/persistence/entity/Spacecraft.java
@@ -4,12 +4,16 @@ import com.alnicode.spaceships.constants.SpacecraftConstants;
 import java.util.Objects;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.ManyToOne;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -18,10 +22,12 @@ import lombok.Setter;
  * The spacecraft entity abstract model.
  *
  */
-@MappedSuperclass
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity
+@Table(name = "spacecrafts")
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Spacecraft {
 
     @Id
