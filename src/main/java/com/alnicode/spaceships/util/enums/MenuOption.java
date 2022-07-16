64a16df3f6dd5033c897e93ce9b6c1c5dae055f1
Alben Bustamante <albenbustamante@gmail.com>
2022-07-16 15:11:52 -0500

    Define a menu options enumeration

---
+++ src/main/java/com/alnicode/spaceships/util/enums/MenuOption.java
@@ -0,0 +1,22 @@
+package com.alnicode.spaceships.util.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * The options to show and use on the main menu.l
+ *
+ * @author Alben Bustamante
+ * @since 1.0
+ * @version 1.0
+ */
+@RequiredArgsConstructor
+@Getter
+public enum MenuOption {
+    REGISTER(1, "Register inventories and spaceships."),
+    CONSULT(2, "Search or consult inventories and spaceships."),
+    EXIT(3, "Exit");
+
+    private final int option;
+    private final String message;
+}
