892bb832c8403cb31efc38c6572b20d64d5bc91b
Alben Bustamante <albenbustamante@gmail.com>
2022-07-15 19:22:00 -0500

    Define some swagger constants

---
+++ src/main/java/com/alnicode/spaceships/util/constants/SwaggerConstants.java
@@ -0,0 +1,17 @@
+package com.alnicode.spaceships.util.constants;
+
+/**
+ * The swagger constants.
+ *
+ * @author Alben BUstamante
+ * @since 1.0
+ * @version 1.0
+ */
+public interface SwaggerConstants {
+    String TITLE = "Spaceships";
+    String DESCRIPTION = "An REST API for a spaceships system made with love for Sofka :D";
+    String URL = "https://github.com/alnicode";
+    String EMAIL = "albenbustamante@gmail.com";
+    String NAME = "Alben Bustamante";
+    String PACKAGE = "com.alnicode.spaceships.web.controller";
+}
