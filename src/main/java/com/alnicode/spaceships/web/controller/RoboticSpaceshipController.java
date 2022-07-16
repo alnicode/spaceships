7d5415114c3d19e463733009e307ff6b716076fa
Alben Bustamante <albenbustamante@gmail.com>
2022-07-15 19:11:52 -0500

    Define the robotic spaceship controller

--- src/main/java/com/alnicode/spaceships/web/controller/RoboticSpaceshipController.java
+++ src/main/java/com/alnicode/spaceships/web/controller/RoboticSpaceshipController.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,29 +34,61 @@ public class RoboticSpaceshipController extends ThrustSpaceshipController<Roboti
         return service;
     }
 
+    /**
+     * Get all the spaceships with the same image status.
+     *
+     * @param imageStatus the image status to search.
+     * @return a response entity with the spaceships.
+     */
     @GetMapping("/image-status/{imageStatus}")
     public ResponseEntity<List<RoboticSpaceshipResponse>> getByImageStatus(@PathVariable("imageStatus") boolean imageStatus) {
         return ResponseEntity.of(service.getByImageStatus(imageStatus));
     }
 
+    /**
+     * Get all the spaceships with the same speed.
+     *
+     * @param speed the speed to search.
+     * @return a response entity with the spaceships.
+     */
     @GetMapping("/speed/{speed}")
     public ResponseEntity<List<RoboticSpaceshipResponse>> getBySpeed(@PathVariable("speed") String speed) {
         return ResponseEntity.of(service.getBySpeed(speed));
     }
 
+    /**
+     * Get all the spaceships with the same destiny orbit.
+     *
+     * @param destinyOrbit the destiny orbit to search.
+     * @return a response entity with the spaceships.
+     */
     @GetMapping("/destiny/{destinyOrbit}")
     public ResponseEntity<List<RoboticSpaceshipResponse>> getByDestinyOrbit(@PathVariable("destinyOrbit") String destinyOrbit) {
         return ResponseEntity.of(service.getByDestinyOrbit(destinyOrbit));
     }
 
-    @GetMapping("/touch/{name}")
-    public ResponseEntity<Object> touchDown(@PathVariable("name") String name) {
-        return null;
+    /**
+     * The spaceship lands.
+     *
+     * @param request the request to land.
+     * @return an empty response entity.
+     */
+    @PostMapping("/touch-down")
+    public ResponseEntity<Object> touchDown(@RequestBody RoboticSpaceshipRequest request) {
+        service.touchDown(request);
+        return ResponseEntity.noContent().build();
     }
 
-    @GetMapping("/")
-    public ResponseEntity<Object> sendImages() {
-        return null;
+    /**
+     * The spaceship send images.
+     *
+     * @param request the request for send images.
+     * @return an empty response entity.
+     */
+    @PostMapping("/send-images")
+    public ResponseEntity<Object> sendImages(@RequestBody RoboticSpaceshipRequest request) {
+        service.sendImages(request);
+        return ResponseEntity.noContent().build();
     }
 
 }
